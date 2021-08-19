355cb35cb48cbbed7ec52c7f507d54c41ede47cf
MSI\Jim <jim60308@gmail.com>
2021-08-19 11:36:27 +0800

    Deal model

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from src.model import Deal, Build, Land, Park
+
 from src.holder import Holder
+
+from src.model.Deal import Deal
+from src.model.Build import Build
+from src.model.Land import Land
+from src.model.Park import Park
 
 import pandas as pd 
 import numpy as np 
 
+from typing import List
+
+
+def readDealFile(path: str) -> List[Deal]:
+    output: List[Deal] = []
+    data = pd.read_csv(path, encoding="utf8")
+    for index, row in data.iterrows():
+        output.append(Deal(row))
+    return output
+
 
 if __name__ == "__main__":
-    data = pd.read_csv("data/a_lvr_land_a.csv", encoding="utf8")
-    for index, row in data.iterrows():
-        deal = Deal.Deal(row)
-        # print(index)
+
+    dealArrays = readDealFile("data/a_lvr_land_a.csv")
         
 
 
--- src/holder/Holder.py
+++ src/holder/Holder.py
@@ -1,11 +1,16 @@
-from ..model import Deal, Build, Land, Park
+
+from ..model.Deal import Deal
+from ..model.Build import Build
+from ..model.Land import Land
+from ..model.Park import Park
+
 from typing import List
 
 class Holder():
     deal: Deal
     ###
-    build_list: List
-    land_list: List
-    park_list: List
+    builds: List[Build]
+    lands: List[Land]
+    parks: List[Park]
 
 
